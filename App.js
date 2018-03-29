d92d0f4a85514cb7331df1f0470fc9608a58a4ec
Brian Gerhards <bgerhards@pillartechnology.com>
2018-03-29 11:04:13 -0500

    add flow support

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+//@flow
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -5,9 +7,6 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
